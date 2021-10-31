de4212082dd2c8c5b0e664e313b81a720958a931
Prakhar <prakhar_fw10_014@masai.school>
2021-10-31 12:24:56 +0530

    Updates

--- precache-manifest.4d23091b65b2f8b9aae6e9eab6ee459b.js
+++ precache-manifest.4d23091b65b2f8b9aae6e9eab6ee459b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e8eee8302788adcaa5df5c18ebe97fe6",
+    "revision": "2f2c75e5862a97f92cd01b33669cfcd3",
     "url": "/portfolio2/index.html"
   },
   {
-    "revision": "d6c8c43534325a47065e",
-    "url": "/portfolio2/static/css/main.1852e170.chunk.css"
+    "revision": "bb0414bf2c2ce083d5c0",
+    "url": "/portfolio2/static/css/main.1f4d7554.chunk.css"
   },
   {
     "revision": "ff0a3351b79b742f0f23",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio2/static/js/2.8d293839.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d6c8c43534325a47065e",
+    "revision": "bb0414bf2c2ce083d5c0",
     "url": "/portfolio2/static/js/main.3f603b34.chunk.js"
   },
   {
